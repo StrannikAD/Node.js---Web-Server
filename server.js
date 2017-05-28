6e67f625d7187e7317832d949280f6c752240370
Anna <a.dimitrova.au@gmail.com>
2017-05-29 09:39:12 +1000

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+var app = express(); // calls express method and assigns it to the app variable
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>'); // sends response to HTTP request
+  res.send({
+    name: 'Anna',
+    likes: [
+      'Traveling',
+      'Gadgets'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000); // binds application to our machine
+
