baf19b68979ad92424a81addb9f1fcaa7b28baa1
Anna <a.dimitrova.au@gmail.com>
2017-05-29 11:27:24 +1000

    Added handlebars for static page template functionality

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express(); // calls express method and assigns it to the app variable
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public')); // takes absolute path
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>'); // sends response to HTTP request
-  res.send({
-    name: 'Anna',
-    likes: [
-      'Traveling',
-      'Gadgets'
-    ]
+  // res.send({
+  //   name: 'Anna',
+  //   likes: [
+  //     'Traveling',
+  //     'Gadgets'
+  //   ]
+  // });
+  res.render('home.hbs', {
+  pageTitle: 'Home Page',
+  welcomeMessage: 'Welcome to my website',
+  currentYear: new Date().getFullYear()
   });
 });
 
+
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
@@ -25,5 +36,7 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000); // binds application to our machine
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+}); // binds application to our machine
 
